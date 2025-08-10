51d71061ab23b518a93555a7de1112cac37d47df
v-spnz <v.spence014@gmail.com>
2025-08-10 19:38:48 +1200

    refractor: updated package, theme, and app name for AI Budget App

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.aibudgetapp"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.aibudgetapp"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
